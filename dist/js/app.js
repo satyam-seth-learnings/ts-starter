6fe78f4d08cd27f9156adf212f084de7a41e5222
satyam-seth <satyam1998.1998@gmail.com>
2023-01-03 23:06:10 +0530

    fix

---
+++ dist/js/app.js
@@ -0,0 +1,12 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const jsdomDemo2_1 = __importDefault(require("./jsdomDemo2"));
+window.onload = () => {
+    console.log('demo app');
+    const examplePage = new jsdomDemo2_1.default('page');
+    examplePage.build();
+};
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYXBwLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vLi4vc3JjL3RzL2FwcC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7OztBQUFBLDhEQUF1QztBQUV2QyxNQUFNLENBQUMsTUFBTSxHQUFHLEdBQUcsRUFBRTtJQUNuQixPQUFPLENBQUMsR0FBRyxDQUFDLFVBQVUsQ0FBQyxDQUFDO0lBRXhCLE1BQU0sV0FBVyxHQUFHLElBQUksb0JBQVcsQ0FBQyxNQUFNLENBQUMsQ0FBQztJQUM1QyxXQUFXLENBQUMsS0FBSyxFQUFFLENBQUM7QUFDdEIsQ0FBQyxDQUFDIn0=
